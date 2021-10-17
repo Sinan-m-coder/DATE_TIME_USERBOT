eac80ab842a5bbddb11a8d1e6df933e15e2879c1
Sinan-m-coder <92448695+Sinan-m-coder@users.noreply.github.com>
2021-10-17 16:44:07 +0530

    Update date_time_userbot.py

--- date_time_userbot.py
+++ date_time_userbot.py
@@ -6,8 +6,8 @@
 
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from lists_sinzz.quotes_sinzz import *
-from lists_sinzz.emojis_sinzz import *
+from lists_teletips.quotes_teletips import *
+from lists_teletips.emojis_teletips import *
 from PIL import Image, ImageDraw, ImageFont
 import datetime
 import pytz
@@ -27,23 +27,23 @@ async def main_sinzz():
     try:
         while True:
             if Date_Time_Userbot.is_connected:
-                Quotes_sinzz = random.choice(quotes_sinzz)
-                Emojis_sinzz = random.choice(emojis_sinzz)
-                TimeZone_sinzz = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
-                Time_sinzz = TimeZone_sinzz.strftime("%I:%M %p")
-                Date_sinzz = TimeZone_sinzz.strftime("%b %d") 
-                Image_sinzz = Image.open("image.jpg")
-                Image_font_sinzz = ImageFont.truetype("439415", 360)
-                Image_text_sinzz = f"{Time_sinzz}"
-                Image_edit_sinzz = ImageDraw.Draw(Image_sinzz)
-                Image_edit_sinzz.text((690, 550), Image_text_sinzz, (0, 255, 255), font = Image_font_sinzz)
+                Quotes_teletips = random.choice(quotes_teletips)
+                Emojis_teletips = random.choice(emojis_teletips)
+                TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
+                Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
+                Date_teletips = TimeZone_teletips.strftime("%b %d") 
+                Image_teletips = Image.open("image.jpg")
+                Image_font_teletips = ImageFont.truetype("439415", 360)
+                Image_text_teletips = f"{Time_teletips}"
+                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
+                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_sinzz)
                 Image_sinzz.save("Image_final_teletips.jpg")
-                await Date_Time_Userbot_sinzz.update_profile(bio = f"{Emojis_sinzz} {Quotes_sinzz}" , last_name = f"| ⏰ {Time_sinzz} | 📅 {Date_sinzz}")
-                await Date_Time_Userbot_sinzz.set_profile_photo(photo="Image_final_teletips.jpg")
-                me = await Date_Time_Userbot_sinzz.get_me()
-                photos = await Date_Time_Userbot_sinzz.get_profile_photos("me")
+                await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_sinzz} {Quotes_sinzz}" , last_name = f"| ⏰ {Time_sinzz} | 📅 {Date_sinzz}")
+                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
+                me = await Date_Time_Userbot_teletips.get_me()
+                photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
                 try:
-                    await Date_Time_Userbot_sinzz.delete_profile_photos(photos[1].file_id)
+                    await Date_Time_Userbot_teletips.delete_profile_photos(photos[1].file_id)
                 except Exception:
                     pass        
                 print("Profile Updated!")
@@ -52,7 +52,7 @@ async def main_sinzz():
         await asyncio.sleep(e.x)         
 
 print("DATE TIME USERBOT IS ALIVE!")
-asyncio.ensure_future(main_sinzz())
-Date_Time_Userbot_sinzz.run()
+asyncio.ensure_future(main_teletips())
+Date_Time_Userbot_teletips.run()
 
 #Copyright ©️ 2021 TeLe tips. All Rights Reserved
